efe719418e15c333c0f9327cc76f2543b928a94c
diormazabal <164242847+diormazaball@users.noreply.github.com>
2024-10-18 22:13:33 -0300

    QR niño

--- src/app/PaginasNiño/inicio/inicio.page.ts
+++ src/app/PaginasNiño/inicio/inicio.page.ts
@@ -22,6 +22,7 @@ import { IonContent, IonList, IonItem, IonSearchbar, IonLabel, IonCard, IonCardH
 import { addIcons } from 'ionicons';
 import { star, personCircle, chevronUpCircle, document, colorPalette, globe, qrCodeOutline, earthOutline, mapOutline } from 'ionicons/icons';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NotificacionesService } from 'src/app/common/servicios/notificaciones.service';
 
 @Component({
   selector: 'app-inicio-nino',
@@ -29,7 +30,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrls: ['./inicio.page.scss'],
   standalone: true,
-  imports: [IonCard, IonCardTitle, IonButton, IonCardHeader, IonLabel, IonItem, IonList, IonSearchbar, IonContent, CommonModule, RouterLink,]
+  imports: [ZXingScannerModule,IonCard, IonCardTitle, IonButton, IonCardHeader, IonLabel, IonItem, IonList, IonSearchbar, IonContent, CommonModule, RouterLink,]
 })
 export class InicioPage implements OnInit {
   @ViewChild(ZXingScannerComponent) scanner!: ZXingScannerComponent;
@@ -59,80 +60,86 @@ export class InicioPage implements OnInit {
   constructor(
     private animalsService: FirestoreService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private notificacionesService: NotificacionesService
 
   ) { }
 
   ngOnInit(): void {
-
     this.animalsService.getAnimales().subscribe((data: Animal[]) => {
       this.animales = data;
-    })
+      this.animalesOriginal = [...data]; // Guarda el orden original
+    });
 
     this.authService.authState$.subscribe(user => {
       if (user) {
         this.userId = user.uid;
-        // Cargar los animales y luego las reacciones del usuario
         this.loadAnimalsWithReactions();
+
+        // Verificar cuántos animales ha visto el usuario
+        this.animalsService.getAnimalesVistosPorUsuario(this.userId).subscribe((animalesVistos) => {
+          const animalesVistosCount = animalesVistos.length;
+
+          // Verificar si ya se ha mostrado la notificación anteriormente
+          const notificacionMostrada = localStorage.getItem('notificacionMostrada');
+
+          if (animalesVistosCount == 5 && !notificacionMostrada) {
+            this.notificacionesService.mostrarNotificacion(
+              '¡Felicidades!',
+              'Has visto 5 animales. Ahora puedes participar en la trivia.'
+            );
+
+            // Guardar en localStorage que la notificación ya fue mostrada
+            localStorage.setItem('notificacionMostrada', 'true');
+          }
+        });
       }
     });
-
   }
 
   loadAnimalsWithReactions() {
     this.animalsService.getAnimales().subscribe((animales: Animal[]) => {
       this.animales = animales;
+      this.animalesOriginal = [...animales]; // Guarda el orden original
 
-      // Para cada animal, buscamos la reacción del usuario
       this.animales.forEach(animal => {
         this.animalsService.getUserReaction(animal.id, this.userId).subscribe(reaction => {
-          animal.reaccion = reaction ? reaction.reaction : null; // true = like, false = don't like, null = sin reacción
+          animal.reaccion = reaction ? reaction.reaction : null;
         });
       });
     });
   }
 
-  // Método para filtrar animales según el término de búsqueda
-  filterAnimals(event: any) {
-    this.searchTerm = event.target.value.toLowerCase();
-    if (this.searchTerm && this.searchTerm.trim() !== '') {
-      this.filteredAnimals = this.animales.filter(animal =>
-        animal.nombre_comun.toLowerCase().includes(this.searchTerm)
-      );
+  // Alterna entre ordenar por posición y restaurar el orden original
+  toggleOrdenRuta() {
+    if (this.isSortedByMap) {
+      // Restaurar el orden original
+      this.animales = [...this.animalesOriginal];
     } else {
-      this.filteredAnimals = [];
+      // Ordenar por 'posicion_mapa'
+      this.animales.sort((a, b) => a.posicion_mapa - b.posicion_mapa);
     }
+    this.isSortedByMap = !this.isSortedByMap; // Cambiar el estado
   }
 
-  // Método para redirigir a la página de información del animal
-  goToAnimal(animalId: string) {
-    this.router.navigate(['/animal-info-nino', animalId]);
-    this.searchTerm = '';
-    this.filteredAnimals = [];
+  toggleQrScan() {
+    this.isScanning = !this.isScanning;
+    if (!this.isScanning && this.scanner) {
+      this.scanner.reset();
+    }
   }
 
   like(animalId: string) {
     const animal = this.animales.find(a => a.id === animalId);
     if (animal) {
-      // Actualiza el estado de la UI inmediatamente
       animal.reaccion = true;
-
-      // Luego realiza la operación en Firestore
       this.animalsService.getUserReaction(animalId, this.userId).subscribe(existingReaction => {
         if (existingReaction && existingReaction.id) {
-          // Actualiza la reacción en Firestore
           this.animalsService.updateReaction(existingReaction.id, { reaction: true }).subscribe(() => {
             console.log('Reacción actualizada a Like');
           });
         } else {
-          // Crea una nueva reacción en Firestore
-          const reaction: Reaction = {
-            animalId: animalId,
-            userId: this.userId,
-            fecha: new Date(),
-            reaction: true
-          };
-
+          const reaction: Reaction = { animalId, userId: this.userId, reaction: true, fecha:new Date(), };
           this.animalsService.addReaction(reaction).subscribe(() => {
             console.log('Reacción guardada como Like');
           });
@@ -144,25 +151,14 @@ export class InicioPage implements OnInit {
   dontLike(animalId: string) {
     const animal = this.animales.find(a => a.id === animalId);
     if (animal) {
-      // Actualiza el estado de la UI inmediatamente
       animal.reaccion = false;
-
-      // Luego realiza la operación en Firestore
       this.animalsService.getUserReaction(animalId, this.userId).subscribe(existingReaction => {
         if (existingReaction && existingReaction.id) {
-          // Actualiza la reacción en Firestore
           this.animalsService.updateReaction(existingReaction.id, { reaction: false }).subscribe(() => {
             console.log('Reacción actualizada a No me gusta');
           });
         } else {
-          // Crea una nueva reacción en Firestore
-          const reaction: Reaction = {
-            animalId: animalId,
-            userId: this.userId,
-            fecha: new Date(),
-            reaction: false
-          };
-
+          const reaction: Reaction = { animalId, userId: this.userId, reaction: false,fecha:new Date(),};
           this.animalsService.addReaction(reaction).subscribe(() => {
             console.log('Reacción guardada como No me gusta');
           });
@@ -171,7 +167,10 @@ export class InicioPage implements OnInit {
     }
   }
 
-  onCodeResult(result: string) {
+  onCodeResult(event: any) {
+    // Extraer el texto del resultado escaneado
+    const result = event?.text || event || ''; // Depende de cómo se emita el evento, podría estar en 'text' o directamente en 'event'
+
     console.log('Contenido escaneado:', result);
     this.isScanning = false;
 
@@ -196,19 +195,34 @@ export class InicioPage implements OnInit {
   }
 
 
-  extractAnimalId(result: string): string | null {
+    extractAnimalId(result: string): string | null {
     console.log('Procesando URL:', result);
     const regex = /animal-info\/(\w+)/;
     const match = result.match(regex);
     return match ? match[1] : null;
   }
 
-  toggleQrScan() {
-    this.isScanning = !this.isScanning;
-    if (!this.isScanning && this.scanner) {
-      this.scanner.reset();
+
+    // Método para filtrar animales según el término de búsqueda
+  filterAnimals(event: any) {
+    this.searchTerm = event.target.value.toLowerCase();
+    if (this.searchTerm && this.searchTerm.trim() !== '') {
+      this.filteredAnimals = this.animales.filter(animal =>
+        animal.nombre_comun.toLowerCase().includes(this.searchTerm)
+      );
+    } else {
+      this.filteredAnimals = [];
     }
   }
+
+  // Método para redirigir a la página de información del animal
+  goToAnimal(animalId: string) {
+    this.router.navigate(['/animal-info', animalId]);
+    this.searchTerm = '';
+    this.filteredAnimals = [];
+  }
+
+
 
 
 
