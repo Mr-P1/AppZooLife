254cc389439a1e54efe9da5aaf54add19ec0562b
diormazabal <164242847+diormazaball@users.noreply.github.com>
2024-11-19 15:55:32 -0300

    cambios varios

--- src/app/PaginasNiño/info-animal/info-animal.page.ts
+++ src/app/PaginasNiño/info-animal/info-animal.page.ts
@@ -5,7 +5,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, Io
 import { FirestoreService } from '../../common/servicios/firestore.service';
 import { Animal } from '../../common/models/animal.model';
 import { Observable } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../common/servicios/auth.service';
 
 @Component({
@@ -16,35 +16,82 @@ import { AuthService } from '../../common/servicios/auth.service';
   imports: [IonBackButton, IonItem, IonLabel, IonList, IonGrid, IonIcon, IonButtons, IonButton, IonPopover, IonCol, IonRow, IonLoading, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonContent]
 })
 export class InfoAnimalPage implements OnInit {
-
+  private readonly VIEWS_LIMIT = 5; // Límite de vistas de animales antes de notificar
+  private viewTimeout: any; // Variable para almacenar el temporizador
   animal$: Observable<Animal | null> | undefined;
+  alertaVisible = false;
+  alertButtons: any[];
 
   constructor(
     private animalService: FirestoreService,
     private route: ActivatedRoute,
     private authService: AuthService,
-  ) { }
+    private router: Router
+  ){
+    this.alertButtons = [
+      {
+        text: 'Ir a trivia',
+        handler: () => {
+          this.redirigirATrivia();
+        }
+      },
+      {
+        text: 'Cerrar',
+        handler: () => {
+          this.alertaVisible = false; // Cierra la alerta
+        }
+      }
+    ];
+  }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
+    const metodoIngreso = this.route.snapshot.queryParamMap.get('metodo');
     const userId = this.authService.currentUserId;
 
-    if (id) {
+    if (id && metodoIngreso) {
+      // Suscribirse al observable del animal para obtener sus datos
       this.animal$ = this.animalService.getAnimal(id);
 
-      if (userId) {
-        this.animalService.usuarioHaVistoAnimal(userId, id).subscribe(haVisto => {
-          if (!haVisto) {
-            setTimeout(() => {
-              this.animalService.guardarAnimalVisto(userId, id).subscribe({
-                next: () => console.log('Animal visto guardado exitosamente'),
-                error: (error) => console.error('Error al guardar el animal visto', error)
-              });
-            }, 20000);
-          }
-        });
+      // Acceder al área una vez que se obtienen los datos del animal
+      this.animal$.subscribe(animal => {
+        if (animal && userId) {
+          const area = animal.area; // Obtén el área desde el modelo del animal
+          // Guardar el animal visto incluyendo el área
+          this.animalService.guardarAnimalVisto(userId, id, metodoIngreso, area).subscribe({
+            next: () => console.log('Animal visto registrado exitosamente.'),
+            error: (error) => console.error('Error al registrar el animal visto:', error),
+          });
+
+          // Inicia el temporizador para el conteo de vistas de sesión
+          this.viewTimeout = setTimeout(() => {
+            this.incrementarYVerificarVistasSesion(id);
+          }, 2000); // 20 segundos en milisegundos
+        }
+      });
+    }
+  }
+
+  private incrementarYVerificarVistasSesion(animalId: string) {
+    // Recupera el contador de la sesión actual desde localStorage
+    let atraccionesVistasSesion = JSON.parse(localStorage.getItem('atraccionesVistasSesion') || '[]');
+
+    // Si el animal ya fue contado en esta sesión, no lo cuenta de nuevo
+    if (!atraccionesVistasSesion.includes(animalId)) {
+      atraccionesVistasSesion.push(animalId);
+      localStorage.setItem('atraccionesVistasSesion', JSON.stringify(atraccionesVistasSesion));
+
+      // Verifica si el número de animales vistos en la sesión alcanza el límite
+      if (atraccionesVistasSesion.length === this.VIEWS_LIMIT) {
+        console.log('Mostrando alerta automáticamente');
+        this.alertaVisible = true; // Muestra la alerta
       }
     }
+  }
+
+  private redirigirATrivia() {
+    this.alertaVisible = false; // Cierra la alerta
+    this.router.navigate(['/adulto/trivia']); // Redirige a la página de trivia
   }
 
 
--- src/app/PaginasNiño/inicio/inicio.page.ts
+++ src/app/PaginasNiño/inicio/inicio.page.ts
@@ -299,11 +299,11 @@ export class InicioPage implements OnInit {
       }
       this.animalsService.getAnimalById(result).subscribe((animal) => {
         if (animal) {
-          this.router.navigate(['/animal-info-nino', result]);
+          this.router.navigate(['/animal-info-nino', result],{ queryParams: { metodo: 'qr' } });
         } else {
           this.animalsService.getPlantaById(result).subscribe((planta) => {
             if (planta) {
-              this.router.navigate(['/info-plantas-nino', result]);
+              this.router.navigate(['/info-plantas-nino', result],{ queryParams: { metodo: 'qr' } });
             } else {
               console.error('No se encontró información para el ID escaneado.');
             }
